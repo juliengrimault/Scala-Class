a654cc234ff515fa4bc774af09b4ec8226e7baa1
Julien Grimault <grimaultjulien@gmail.com>
2013-11-28 11:37:12 +0800

    Epidemy simulator - week 2 assignement

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -1,6 +1,9 @@
 package simulations
 
 import math.random
+import simulations.gui.EpidemyDisplay.Situation
+import simulations.gui.EpidemyDisplay
+import simulations.gui.Grid
 
 class EpidemySimulator extends Simulator {
 
@@ -12,11 +15,30 @@ class EpidemySimulator extends Simulator {
     val roomColumns: Int = 8
 
     // to complete: additional parameters of simulation
+    val prevalenceRate = 0.01
+    val transmissibility = 0.4
+    val dyingRate = 0.25
+
+    val maxDurationBeforeMove = 5
+
+    val takePlaneProbability = 0
+
+    val incubationLenght = 6
+    val deathLength = 14
+    val partialRecoveryLength = 16
+    val totalRecoveryLength = 18
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (for(i <- 0 until population) yield new Person(i)).toList
+  for (p <- persons) {
+    val infectedOnStart = random < prevalenceRate
+    if (infectedOnStart)
+      p.infect()
+  }
+
+  for(p <- persons) p.enqueueNextTurn()
 
   class Person (val id: Int) {
     var infected = false
@@ -28,8 +50,94 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+
+    def enqueueNextTurn(): Unit =
+      afterDelay(timeBeforeMoving) {
+        if (!dead) {
+          val p = nextPosition
+          val hasMoved = p._1 != row || p._2 != col
+          row = p._1
+          col = p._2
+
+          if (hasMoved && !infected && !immune)
+            infectionPhase()
+
+          enqueueNextTurn()
+        }
+      }
+
+    private def timeBeforeMoving: Int =  randomBelow(maxDurationBeforeMove) + 1
+
+    def nextPosition: (Int, Int) = {
+      val takePlane = random < takePlaneProbability
+
+      if (takePlane)
+        (randomBelow(roomRows), randomBelow(roomColumns))
+      else {
+        val possibleMove = availableMove
+        possibleMove(randomBelow(possibleMove.length))
+      }
+    }
+
+    def availableMove: List[(Int, Int)] = {
+      val left = (row , if (col == 0) roomColumns - 1 else col - 1)
+      val up = (if (row == 0) roomRows - 1 else row - 1, col)
+      val right = (row , (col + 1) % roomColumns)
+      val down = ((row + 1) % roomRows, col)
+
+      val available = List(left, up, right, down)
+      val noSickPeopleInRooms = available filter (location => !hasSickPeopleInRoom(_.sick)(location))
+
+      if (noSickPeopleInRooms.isEmpty)
+        List((row, col))
+      else
+        noSickPeopleInRooms
+    }
+
+    def hasSickPeopleInRoom(predicate: Person => Boolean)(location: (Int, Int)) = location match {
+      case (r,c) => persons.exists (p => p.row == r && p.col == c && predicate(p))
+    }
+
+    def infectionPhase() = {
+      if (hasSickPeopleInRoom(_.infected)((row, col))) {
+        val fallsSick = random < transmissibility
+        if (fallsSick) {
+          infect()
+        }
+      }
+    }
+
+    def infect() = {
+      infected = true
+      incubationPhase()
+      finalPhase();
+    }
+
+    def incubationPhase() = afterDelay(incubationLenght) { sick = true }
+
+
+    def finalPhase() = {
+      afterDelay(deathLength) {
+        val shouldDie = random < dyingRate
+        if (shouldDie)
+          dead = true
+        else {
+          recoveryPhase()
+        }
+      }
+    }
+
+    def recoveryPhase() = {
+      afterDelay(partialRecoveryLength - deathLength) {
+        immune = true
+        sick = false
+      }
+
+      afterDelay(totalRecoveryLength - deathLength) {
+        immune = false
+        infected = false
+      }
+    }
   }
+
 }
